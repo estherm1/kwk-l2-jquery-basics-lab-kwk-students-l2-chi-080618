3dfe157c0b724f28dd803fba4d1ae4ee7378752e
estherm1 <esther.mathunny@gmail.com>
2018-08-13 16:34:38 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -19,6 +19,7 @@ input.addEventListener('keydown', function(event) {
       console.log(event.key)
   if (isNaN(event.key)) {
     return event.preventDefault()
+    alert(this is not a number, but instead it is a string)
   }
 })
 
