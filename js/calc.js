1037f4a391fbe77f9085484038fadc2d98fa21c6
estherm1 <esther.mathunny@gmail.com>
2018-08-13 15:29:51 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -2,3 +2,12 @@ $(document).ready(function(){
   doMath();
 });
 var num1 = $('#number1');
+var num2 = $('#number2');
+var operator = $('#operation');
+
+function doMath(){
+  $('#equals').on('click', function(){
+    num1 = parseFloat(num1.va1());
+    
+  })
+}
