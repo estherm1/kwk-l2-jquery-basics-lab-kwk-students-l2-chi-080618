1249681e82a2453dd375cce248c8493820966113
estherm1 <esther.mathunny@gmail.com>
2018-08-13 16:40:49 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -16,13 +16,13 @@ function doMath(){
 const input = document.querySelector('input')
 
 
-input.addEventListener('keydown', function(event) {
-  if (isNaN(event.key)) {
-    alert("this is not a number, but instead it is a string");
-    return event.preventDefault()
+// input.addEventListener('keydown', function(event) {
+//   if (isNaN(event.key)) {
+//     alert("this is not a number, but instead it is a string");
+//     return event.preventDefault()
     
-  }
-})
+//   }
+// })
 
 
 
