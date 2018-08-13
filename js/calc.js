d201ae036c085b476f8f54d857f51a15506faebc
estherm1 <esther.mathunny@gmail.com>
2018-08-13 16:39:25 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -16,7 +16,7 @@ function doMath(){
 const input = document.querySelector('input')
 
 input.addEventListener('keydown', function(event) {
-  if (isNaN(event.key)) {
+  if (isNaN(num1(event.key))) {
     alert("this is not a number, but instead it is a string");
     return event.preventDefault()
     
